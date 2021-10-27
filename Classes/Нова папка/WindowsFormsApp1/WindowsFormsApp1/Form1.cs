e2937abbcb5580a7e7f95a445e1f484f1e166541
Lera-Buka <79201188+Lera-Buka@users.noreply.github.com>
2021-10-27 13:15:35 +0300

    Add files via upload

---
+++ Classes/Нова папка/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private List<Human> Human_List = new List<Human> { };
+        public int counter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            while (counter<5)
+            {
+                Human_List.Add(new Human());
+                counter += 1;
+            }
+        }
+        public void DataTable()
+
+        {
+            DataTable Table = new DataTable();
+            DataColumn idColumn = new DataColumn("Id");
+            idColumn.AutoIncrement = true;
+            DataColumn Name = new DataColumn("Name");
+            DataColumn Surname = new DataColumn("Surname");
+            DataColumn Age = new DataColumn("Age");
+            DataColumn Gender = new DataColumn("Gender");
+            DataColumn Country = new DataColumn("Country");
+            DataColumn District = new DataColumn("District");
+            DataColumn Street = new DataColumn("Street");
+
+
+
+            Table.Columns.Add(idColumn);
+            Table.Columns.Add(Name);
+            Table.Columns.Add(Surname);
+            Table.Columns.Add(Age);
+            Table.Columns.Add(Gender);
+            Table.Columns.Add(Country);
+            Table.Columns.Add(District);
+            Table.Columns.Add(Street);
+
+            while (counter<5)
+            {
+                Human human = Human_List[counter];
+                Table.Rows.Add(new Object[] { idColumn, human.Name, human.Surname, human.Age, human.Gender, human.Address.Country, human.Address.District, human.Address.Street });
+                human = null;
+                counter += 1;
+
+            }
+        }
+
+    }
+}
---
+++ Classes/Нова папка/WindowsFormsApp1/WindowsFormsApp1/Human.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+  
+    
+        class Human
+        {
+            public string name;
+            public string surname;
+            public int age;
+            public string gender;
+            public Addres address;
+
+            public Human(string Name,string Surname, int Age, string Gender, Addres Address)
+            {
+                name = Name;
+                surname = Surname;
+                age = Age;
+                gender = Gender;
+                address = Address;
+            }
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+
+            }
+            public string Surname
+            {
+                get { return surname; }
+                set { surname = value; }
+
+            }
+            public int Age
+            {
+                get
+                {
+                    return age;
+                }
+                set
+                {
+                    age = value;
+                }
+
+            }
+            public string Gender
+            {
+                get
+                {
+                    return gender;
+                }
+                set
+                {
+                    gender = value;
+                }
+            }
+            public Addres Address
+            {
+                get
+                {
+                    return address;
+                }
+                set
+                {
+                    address = value;
+                }
+
+            }
+
+            public string InputName()
+            {
+                Console.Write("Input name: ");
+                name = Console.ReadLine();
+                return name;
+            }
+            public string InputSurname()
+            {
+                Console.Write("Input surname: ");
+                name = Console.ReadLine();
+                return surname;
+            }
+            public int InputAge()
+            {
+                Console.Write("Input age: ");
+                string ageStr = age.ToString();
+                ageStr = Console.ReadLine();
+                int.TryParse(ageStr, out age);
+                return age;
+            }
+            public string InputGender()
+            {
+                Console.Write("Input gender: ");
+                gender = Console.ReadLine();
+                return gender;
+            }
+            public Addres InputAddres()
+            {
+                Console.Write("Input address:");
+                address = new Addres();
+                return address;
+            }
+            public Human()
+            {
+                name = InputName();
+                age = InputAge();
+                gender = InputGender();
+                address = InputAddres();
+            }
+        }
+}
